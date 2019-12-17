157818d956a4cb42ef558c652b7a1538982209af
kingovchouffe <pierre.anthony.legeay@gmail.com>
2019-12-17 15:09:40 +0100

    bcp mieux mais pas encore

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ void setup() {
   int ypos;
   depart.x=xpos;
   depart.y=ypos;
-  depart.cout=0;
   depart.h=0;
 }
 
@@ -22,5 +21,104 @@ int compareNoeud(noeud n1,noeud n2){
   else return -1;
 }
 void algoAstar(uint8_t table[150][100], noeud objectif, noeud depart){
-
+pente = (objectif.y-depart.y)/(objectif.x-depart.x);
+if(pente>100&&objectif.y<depart.y)dir=0; //cas ou l'objectif est au dessus du robot dans l'axe des y (pente infini)
+if(pente>100&&objectif.y>depart.y)dir=1; //cas ou l'objectif est au dessous du robot dans l'axe des y (pente infini)
+if(pente>0&&objectif.x>depart.x)dir=2; //cas ou l'objectif est en haut à droite du robot
+if(pente>0&&objectif.x<depart.x)dir=3; //cas ou l'objectif est en bas à gauche du robot
+if(pente<0&&objectif.x>depart.x)dir=4; //cas ou l'objectif est bas à droite du robot
+if(pente<0&&objectif.x<depart.x)dir=5; //cas ou l'objectif est en haut à gauche du robot
+switch (dir) {
+  case 0:
+  xcourant= depart.x;
+  ycourant= depart.y;
+  n1.x=xcourant-1;
+  n1.y=ycourant-1;
+  n1.h=(objectif.x-n1.x)*(objectif.x-n1.x)+(objectif.y-n1.y)*(objectif.y-n1.y);
+  n2.x=xcourant;
+  n2.y=ycourant-1;
+  n2.h=(objectif.x-n2.x)*(objectif.x-n2.x)+(objectif.y-n2.y)*(objectif.y-n2.y);
+  n3.x=xcourant+1;
+  n3.y=ycourant-1;
+  n3.h=(objectif.x-n3.x)*(objectif.x-n3.x)+(objectif.y-n3.y)*(objectif.y-n3.y);
+  listeAttente[0]=n1;
+  listeAttente[1]=n2;
+  listeAttente[2]=n3;
+  case 1:
+  xcourant= depart.x;
+  ycourant= depart.y;
+  n1.x=xcourant-1;
+  n1.y=ycourant+1;
+  n1.h=(objectif.x-n1.x)*(objectif.x-n1.x)+(objectif.y-n1.y)*(objectif.y-n1.y);
+  n2.x=xcourant;
+  n2.y=ycourant+1;
+  n2.h=(objectif.x-n2.x)*(objectif.x-n2.x)+(objectif.y-n2.y)*(objectif.y-n2.y);
+  n3.x=xcourant+1;
+  n3.y=ycourant+1;
+  n3.h=(objectif.x-n3.x)*(objectif.x-n3.x)+(objectif.y-n3.y)*(objectif.y-n3.y);
+  listeAttente[0]=n1;
+  listeAttente[1]=n2;
+  listeAttente[2]=n3;
+  case 2:
+  xcourant= depart.x;
+  ycourant= depart.y;
+  n1.x=xcourant;
+  n1.y=ycourant-1;
+  n1.h=(objectif.x-n1.x)*(objectif.x-n1.x)+(objectif.y-n1.y)*(objectif.y-n1.y);
+  n2.x=xcourant+1;
+  n2.y=ycourant-1;
+  n2.h=(objectif.x-n2.x)*(objectif.x-n2.x)+(objectif.y-n2.y)*(objectif.y-n2.y);
+  n3.x=xcourant+1;
+  n3.y=ycourant;
+  n3.h=(objectif.x-n3.x)*(objectif.x-n3.x)+(objectif.y-n3.y)*(objectif.y-n3.y);
+  listeAttente[0]=n1;
+  listeAttente[1]=n2;
+  listeAttente[2]=n3;
+  case 3:
+  xcourant= depart.x;
+  ycourant= depart.y;
+  n1.x=xcourant-1;
+  n1.y=ycourant;
+  n1.h=(objectif.x-n1.x)*(objectif.x-n1.x)+(objectif.y-n1.y)*(objectif.y-n1.y);
+  n2.x=xcourant-1;
+  n2.y=ycourant+1;
+  n2.h=(objectif.x-n2.x)*(objectif.x-n2.x)+(objectif.y-n2.y)*(objectif.y-n2.y);
+  n3.x=xcourant;
+  n3.y=ycourant+1;
+  n3.h=(objectif.x-n3.x)*(objectif.x-n3.x)+(objectif.y-n3.y)*(objectif.y-n3.y);
+  listeAttente[0]=n1;
+  listeAttente[1]=n2;
+  listeAttente[2]=n3;
+  case 4:
+  xcourant= depart.x;
+  ycourant= depart.y;
+  n1.x=xcourant;
+  n1.y=ycourant+1;
+  n1.h=(objectif.x-n1.x)*(objectif.x-n1.x)+(objectif.y-n1.y)*(objectif.y-n1.y);
+  n2.x=xcourant+1;
+  n2.y=ycourant+1;
+  n2.h=(objectif.x-n2.x)*(objectif.x-n2.x)+(objectif.y-n2.y)*(objectif.y-n2.y);
+  n3.x=xcourant+1;
+  n3.y=ycourant;
+  n3.h=(objectif.x-n3.x)*(objectif.x-n3.x)+(objectif.y-n3.y)*(objectif.y-n3.y);
+  listeAttente[0]=n1;
+  listeAttente[1]=n2;
+  listeAttente[2]=n3;
+  case 5:
+  xcourant= depart.x;
+  ycourant= depart.y;
+  n1.x=xcourant-1;
+  n1.y=ycourant;
+  n1.h=(objectif.x-n1.x)*(objectif.x-n1.x)+(objectif.y-n1.y)*(objectif.y-n1.y);
+  n2.x=xcourant-1;
+  n2.y=ycourant-1;
+  n2.h=(objectif.x-n2.x)*(objectif.x-n2.x)+(objectif.y-n2.y)*(objectif.y-n2.y);
+  n3.x=xcourant;
+  n3.y=ycourant-1;
+  n3.h=(objectif.x-n3.x)*(objectif.x-n3.x)+(objectif.y-n3.y)*(objectif.y-n3.y);
+  listeAttente[0]=n1;
+  listeAttente[1]=n2;
+  listeAttente[2]=n3;
+}
+if(table[n1.x][n1.y]=!1){}
 }
--- src/pathfindingTeensy32.h
+++ src/pathfindingTeensy32.h
@@ -3,8 +3,23 @@
 typedef struct{
   uint8_t x; //position x,y du noeud
   uint8_t y;
-  int cout; // cout du noeud
-  int h; // heuristique
+  float h; // heuristique
 }noeud;
 
 uint8_t table[150][100];
+
+noeud listeRetenue[100]; //liste retenue de noeud
+noeud listeAttente[3];
+noeud n1;
+noeud n2;
+noeud n3;
+
+
+float pente;
+uint8_t dir;
+
+uint8_t xcourant;
+uint8_t ycourant;
+
+uint8_t xparent;
+uint8_t yparent;
